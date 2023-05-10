e2340c68519a28088153af5bf5a3841d32663515
Yamin-Ara <yamin.ara@g.bracu.ac.bd>
2023-05-10 21:20:01 +0600

    used postman

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Union
 
 from fastapi import FastAPI
+import random as rd
 
 app = FastAPI()
 
@@ -14,5 +15,28 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
-# pd to the code file then use python -m uvicorn filename:app(variable used to call FastApi) --reload
+items = {
+    "1": {"name": "Foo", "price": 50.2},
+    "2": {"name": "Bar", "price": 62},
+    "3": {"name": "Baz", "price": 50.8},
+    "4": {"name": "Too", "price": 54.3},
+    "5": {"name": "Boo", "price": 20.6},
+    "6": {"name": "Van", "price": 10.9},
+}
+
+# endpoint in postman: http://localhost:5000/item
+@app.get("/item")
+async def read_item():
+    random_key = rd.choice(list(items.keys())) ## randomly selecting a key in items
+    random_value = items[random_key]  ## calling the value of the selected key
+    return {"message": random_value}  ## returning the value
+
+# endpoint in postman: http://localhost:5000/items 
+@app.get("/items")
+async def read_items():
+    return items     ## returning all the value
+
+# start the app → uvicorn index:app --port 5000 --reload --log-level debug    
+
+# cd to the code file then use python -m uvicorn filename:app(variable used to call FastApi) --reload
 # example: PS D:\python_stuff> python -m uvicorn main:app --reload     
