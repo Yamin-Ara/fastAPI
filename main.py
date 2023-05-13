22bd0074f1472cc1e212f48f43f32f72e215f958
Yamin-Ara <yamin.ara@g.bracu.ac.bd>
2023-05-14 01:42:11 +0600

    post operation

--- main.py
+++ main.py
@@ -1,40 +1,45 @@
 from typing import Union
-
+from pydantic import BaseModel # to instruct the api to build a schema
+from typing import Optional  # to handle any data type into our desired type
 from fastapi import FastAPI
-import random as rd
 
 app = FastAPI()
 
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+class Customer(BaseModel):
+    id: str
+    name: Optional[str] = None
+    email: Optional[str] = None
+    phone: int
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-items = {
-    "1": {"name": "Foo", "price": 50.2},
-    "2": {"name": "Bar", "price": 62},
-    "3": {"name": "Baz", "price": 50.8},
-    "4": {"name": "Too", "price": 54.3},
-    "5": {"name": "Boo", "price": 20.6},
-    "6": {"name": "Van", "price": 10.9},
-}
 
 # endpoint in postman: http://localhost:5000/item
-@app.get("/item")
+@app.get("/")
 async def read_item():
-    random_key = rd.choice(list(items.keys())) ## randomly selecting a key in items
-    random_value = items[random_key]  ## calling the value of the selected key
-    return {"message": random_value}  ## returning the value
+    return {"message": "hello world"}
 
-# endpoint in postman: http://localhost:5000/items 
-@app.get("/items")
-async def read_items():
-    return items     ## returning all the value
+# endpoint in postman: http://localhost:5000/post 
+@app.post("/post")
+async def create_post(items: Item):
+    received = items
+    return {
+        "status": "success",
+        "data": received
+    }
+@app.post("/customer")
+async def create_customer(items: Customer):
+    received = items
+    return {
+        "status": "success",
+        "data": received
+    }
+
+
 
 # start the app → uvicorn index:app --port 5000 --reload --log-level debug    
 
